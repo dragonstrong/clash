c63453b197c0c40e0d2ed209388922ef4a683ccd
dragonstrong <32926995+dragonstrong@users.noreply.github.com>
2021-07-24 15:11:48 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#自动配置脚本
+git clone https://github.com/dragonstrong/clash
+cd clash
+sudo chmod a+x clash
+sudo ./clash -d .
+#export http_proxy=http://127.0.0.1:7890
+#export https_proxy=http://127.0.0.1:7890
+#export socks_proxy=http://127.0.0.1:7891
