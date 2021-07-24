4e9f3e1f4f1180f0ebef1355e9a93ed4143fd8b0
dragonstrong <32926995+dragonstrong@users.noreply.github.com>
2021-07-24 16:50:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #自动配置脚本，先git clash文件夹,然后运行start.sh脚本
 sudo chmod a+x clash
-sudo ./clash -d .
 sudo chmod a+x proxy.sh   
-gnome-terminal -x bash -c "bash proxy.sh;exec bash;" #打开新终端配置系统代理
+gnome-terminal -x bash -c "bash proxy.sh;exec bash;" #打开新终端配置系统代理，好像无效，那就手动输入吧
+sudo ./clash -d .
+
 
